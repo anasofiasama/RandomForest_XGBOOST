045709b50eb0a54709a9cd27b4a282447358a327
Ana Sofía <anasofiasama@gmail.com>
2022-07-13 21:27:06 +0000

    RF

--- src/app.py
+++ src/app.py
@@ -1,13 +1,28 @@
-# Se corre el archivo de jupyter directamente
 
-import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor
+url='https://raw.githubusercontent.com/4GeeksAcademy/random-forest-project-tutorial/main/titanic_train.csv'
+df = pd.read_csv(url, index_col=[0,3])
 
-filename = '/workspace/Logistic-Regression-Project-Tutorial/src/explore.ipynb'
+df=df.drop(columns='Cabin')
 
-with open(filename) as ff:
-    nb_in = nbformat.read(ff, nbformat.NO_CONVERT)
-    
-ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+df['Age']=df['Age'].fillna(df['Age'].mean())
+df['Embarked']=df['Embarked'].fillna(df['Embarked'].mode()[0])
 
-nb_out = ep.preprocess(nb_in)
+df[['Sex','Embarked','Survived']]=df[['Sex','Embarked','Survived']].astype('category')
+
+y=df['Survived']
+X=df.drop(columns=['Ticket','Survived']).copy()
+
+X['Sex']=X['Sex'].cat.codes
+X['Embarked']=X['Embarked'].cat.codes
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=1107)
+
+best_clas=RandomForestClassifier(n_estimators= 1550,min_samples_split= 2,min_samples_leaf= 2,max_depth= 10,criterion= 'gini',
+ bootstrap= True,class_weight=None)
+
+ best_clas.fit(X_train,y_train)
+ y_pred_best=best_clas.predict(X_test)
+
+ import pickle
+filename = '/workspace/RandomForest/models/finalized_model.sav'
+pickle.dump(best_clas, open(filename, 'wb'))
